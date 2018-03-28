6381d5675bbe15a27de7e18dbe6184081c97d613
31632521 <31632521@099218-P31S202.MackSP.br>
2018-03-28 11:26:46 -0300

    Resolução do problema 1

--- src/CalculoIMCJ.java
+++ src/CalculoIMCJ.java
@@ -8,11 +8,6 @@ public class CalculoIMCJ {
         float altura = 0;
         float imc;
         String classifica = null;
-        String sexo = null;
-
-        while (!"f".equalsIgnoreCase(sexo) && !"m".equalsIgnoreCase(sexo)) {
-            sexo = JOptionPane.showInputDialog("Qual seu sexo? (F ou M):");
-        }
 
         while (peso <= 0) {
             peso = Float.parseFloat(JOptionPane.showInputDialog("Seu peso(com ponto):"));
@@ -24,7 +19,6 @@ public class CalculoIMCJ {
 
         imc = (float) (peso / Math.pow(altura, 2));
 
-        if ("m".equalsIgnoreCase(sexo)) {
             if (imc <= 19) {
                 classifica = "Abaixo do Peso";
             } else if (imc <= 25) {
@@ -36,21 +30,7 @@ public class CalculoIMCJ {
             } else {
                 classifica = "Obesidade";
             }
-        }
 
-        if ("f".equalsIgnoreCase(sexo)) {
-            if (imc <= 19.10) {
-                classifica = "Abaixo do Peso";
-            } else if (imc > 19.10 && imc <= 25.89) {
-                classifica = "Peso ideal";
-            } else if (imc >= 25.90 && imc <= 27.39) {
-                classifica = "Pouco acima do Peso";
-            } else if (imc >= 27.40 && imc <= 32.39) {
-                classifica = "Acima do Peso";
-            } else {
-                classifica = "Obesidade";
-            }
-        }
         JOptionPane.showMessageDialog(null, "Seu IMC= " + imc + " - " + classifica);
     }
 }
